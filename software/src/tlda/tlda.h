9ad2c2c463c11058778270205bf84170e2a06966
pavmeh <pavmeh@gmail.com>
2016-06-07 16:41:25 -0700

    Delete tlda.h

--- software/src/tlda/tlda.h
+++
@@ -1,9 +0,0 @@
-/* Hardware drawline */
-void tlda_draw (unsigned int x0,
-                unsigned int y0,
-                unsigned int x1,
-                unsigned int y1,
-                unsigned int color,
-                unsigned int thickness);
-
-void tlda_set_drawing_buffer (void *buffer);
